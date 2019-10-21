f67c95646c60b0c460066ee2378c33b706a3ad09
Udit Rawat <Udit.Rawat@rishabh.com>
2019-10-21 17:11:47 +0530

    route files name changed

--- routes/User.php
+++ routes/User.php
@@ -3,4 +3,5 @@
 Auth::routes();
 Route::prefix('user')->group(function(){
     Route::get('/', 'UserController@index')->name('users.index');
+    Route::get('/show', 'UserController@show')->name('users.show');
 });
